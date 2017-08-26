6956a30c5f003cb73be55232255a550ea4d4e3c8
GCostaSM <gabriel.mei@uol.com.br>
2017-08-26 01:55:30 -0300

    iniciando configuração da api

---
+++ server.js
@@ -0,0 +1,15 @@
+var express  = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.get('/',function(req,res){
+    res.json({"testando":"produto"});
+});
+
+
+
+
+
+app.listen('3000',function(){
+    console.log("Servidor Iniciado");
+});
