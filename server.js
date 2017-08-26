dceecb3effb0e837f85eb3935d580ddc03119b23
GCostaSM <gabriel.mei@uol.com.br>
2017-08-26 17:55:53 -0300

    adicionando rotas de camisas no servidor

--- server.js
+++ server.js
@@ -1,12 +1,33 @@
 var express  = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var mysql      = require('mysql');
 
-app.get('/',function(req,res){
-    res.json({"testando":"produto"});
+
+app.get('/cor',function(req,res){
+  var con = mysql.createConnection('mysql://root:root@localhost:3306/teste');
+  con.query('select * from cor_camisetas',function(err,result,fields){
+    res.json(result);
+  });
+  con.end();
 });
 
+app.get('/tamanho',function(req,res){
+  var con = mysql.createConnection('mysql://root:root@localhost:3306/teste');
+  con.query('select * from tam_camisetas',function(err,result,fields){
+    res.json(result);
+  });
+  con.end();
+});
 
-app.listen('3000',function(){
+app.get('/tipo',function(req,res){
+  var con = mysql.createConnection('mysql://root:root@localhost:3306/teste');
+  con.query('select * from tipo_camisetas',function(err,result,fields){
+    res.json(result);
+  });
+  con.end();
+});
+
+app.listen('9090',function(){
     console.log("Servidor Iniciado");
 });
